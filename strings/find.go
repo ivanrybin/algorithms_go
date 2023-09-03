344f88b0f935f78f2880051947d13c45ccf8a1b1
Ivan Rybin <44673154+ivanrybin@users.noreply.github.com>
2023-09-03 10:34:58 +0200

    strings: find.go

--- strings/find.go
+++ strings/find.go
@@ -2,7 +2,21 @@ package strings
 
 import "fmt"
 
-// FindByZFunction finds entry of p in s O(|p+s|) /  O(|p+s|) (mem / time).
+// KMP Knuth–Morris–Pratt algorithm finds entry of p in s for O(|p+s|) /  O(|p+s|) (mem / time).
+var KMP = FindByPrefixFunction
+
+// FindByPrefixFunction finds entry of p in s for O(|p+s|) /  O(|p+s|) (mem / time).
+func FindByPrefixFunction(s, p string) int {
+	pi := PrefixFunction(fmt.Sprintf("%s#%s", p, s))
+	for i := len(p) + 1; i < len(pi); i++ {
+		if pi[i] == len(p) {
+			return i - len(p) - pi[i]
+		}
+	}
+	return -1
+}
+
+// FindByZFunction finds entry of p in s for O(|p+s|) /  O(|p+s|) (mem / time).
 func FindByZFunction(s, p string) int {
 	z := ZFunction(fmt.Sprintf("%s#%s", p, s))
 	for i := len(p) + 1; i < len(z); i++ {
